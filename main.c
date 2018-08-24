34ee2cc0fc6f24b0cf5d4c1fd69eaf357810eee7
alumno <alumno@lab6pc0>
2018-08-23 21:11:47 -0300

    probando cambios en git

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main()
 
     for(i=0;i<10;i++)
     {
-        printf("Ingrese un numero:");
+        printf("Ingrese un numero sdsdsdsd:");
         scanf("%d",&numero);
 
         if(i==0)
@@ -24,23 +24,7 @@ int main()
         {
             numeroMaximo=numero;
         }
-        if(numero<numeroMinimo)           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
-           <command> [<args>]
-
-These are common Git commands used in various situations:
-
-start a working area (see also: git help tutorial)
-   clone      Clone a repository into a new directory
-   init       Create an empty Git repository or reinitialize an existing one
-
-work on the current change (see also: git help everyday)
-   add        Add file contents to the index
-   mv         Move or rename a file, a directory, or a symlink
-   reset      Reset current HEAD to the specified state
-   rm         Remove files from the working tree and from the index
-
-examine the history and state (see also: git help revisions)
-   bisect     Use binary search to find the commit that introduced a b
+        if(numero<numeroMinimo)
         {
             numeroMinimo=numero;
         }
